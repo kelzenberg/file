2c9ccaf3c72e934705e27cc4583b932414e36eb8
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2020-07-09 18:59:54 +0200

    init State type & read contents of directory

---
+++ main.hs
@@ -0,0 +1,58 @@
+import System.Directory as Directory
+import System.FilePath.Posix as Path
+import Data.List as List
+
+{-
+folderpath -> getDirectoryContent
+     \_________________/
+             V
+           State
+
+printState
+
+awaitUserAction State
+    // perform operation on state
+    // return new state
+    // repeat loop
+-}
+
+data State = State {
+  path :: FilePath,
+  content :: [(String, Bool)], -- (file name, isFolder)
+  selection :: Int
+} deriving (Show)
+
+-- a function that inistalizes the state
+initState :: IO State
+initState = Directory.getHomeDirectory >>= \dir -> return (State dir [] 0)
+
+-- a function that takes a folder path and returns files and folders
+--                                  folders   files
+getFolderContent :: FilePath -> IO [(String, Bool)]
+getFolderContent path = Directory.listDirectory path >>= \content -> return (sort $ map (\fileName -> (fileName, False)) $ map Path.takeFileName content)
+
+-- a function that takes a state and replaces the files and folders in it with the actual data from the drive
+updateStateContent :: State -> IO State
+updateStateContent state = getFolderContent (path state) >>= \content -> return (State (path state) content (validSelection state content))
+  -- validSelection takes the old state and the loaded content of the directory
+  -- and checks weather the selection index in the old state is still valid with the new amount of files and folders
+  -- if the index would be too large it returns 0
+  where validSelection :: State -> [(String, Bool)] -> Int
+        validSelection state content | (selection state) >= (length (content !! 0)) + (length (content !! 1)) = 0
+                                     | otherwise                                                              = (selection state)
+
+-- a function that takes a state and prints in on the screen
+printState :: State -> IO State
+printState state = putStrLn (path state) >> contentPrinter (content state) >> return state
+  where
+    -- todo print files and folder differently
+    contentPrinter :: [(String, Bool)] -> IO ()
+    contentPrinter [] = return ()
+    contentPrinter (x:xs) = putStrLn (fst x) >> contentPrinter xs
+
+-- a function that waits for a user action and performs that action on the state and on the drive
+
+main = initState >>= updateStateContent >>= mainLoop
+
+mainLoop :: State -> IO ()
+mainLoop state = printState state >> return () -- >>= mainLoop state
