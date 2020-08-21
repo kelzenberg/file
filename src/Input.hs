89bf13e1e6a5780043efc28492a882f45fd2df8a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2020-08-21 15:44:04 +0200

    add create new file action & prepare delete and rename

--- src/Input.hs
+++ src/Input.hs
@@ -15,21 +15,47 @@ import System.FilePath.Posix
 returnWithoutExit :: DirState -> IO (DirState, Bool)
 returnWithoutExit state = return (state, False)
 
+createNewFile :: DirState -> IO (DirState)
+createNewFile state = getUserResponse "Enter File Name"
+  >>= (\name -> 
+      if name == "" then return ()
+      else
+        doesFileExist (getPath state)
+        >>= \isFile -> if isFile then return ()
+        else
+          writeFile (joinPath [getPath state, name]) "this is debug content")
+  >> return (state)
+
 createNewDirectory :: DirState -> IO (DirState)
 createNewDirectory state = getUserResponse "Enter Folder Name"
   >>= (\name -> 
-      if name == "" then
-        return ()
+      if name == "" then return ()
       else 
         createDirectoryIfMissing False (joinPath [getPath state, name]))
   >> return (state)
 
+deleteSelectedFile :: DirState -> IO (DirState)
+deleteSelectedFile state = removeFile (joinPath [getPath state, getSelectionName state]) >> return state
+
+renameSelectedFile :: DirState -> IO (DirState)
+renameSelectedFile state = getUserResponse "Enter New Name"
+  >>= (\name -> 
+      if name == "" then return ()
+      else
+        doesFileExist (joinPath [getPath state, name])
+        >>= (\exists -> 
+          if exists then return ()
+          else
+            renameFile (joinPath [getPath state, getSelectionName state]) (joinPath [getPath state, name])))
+      >> return (state)
+        
+
 applyAction :: DirState -> [Char] -> IO (DirState, Bool)
 applyAction state "q" = return (state, True) -- quit ☑️
-applyAction state "n" = return (state, False) -- new file ❌
-applyAction state "N" = createNewDirectory (state) >>= (\newState -> return (newState, False)) -- new folder ☑️
-applyAction state "r" = return (state, False) -- rename ❌
-applyAction state "d" = return (state, False) -- delete ❌
+applyAction state "n" = createNewFile state >>= returnWithoutExit -- new file ❌
+applyAction state "N" = createNewDirectory state >>= \newState -> return (newState, False) -- new folder ☑️
+applyAction state "r" = renameSelectedFile state >>= returnWithoutExit -- rename ❌
+applyAction state "d" = deleteSelectedFile state >>= returnWithoutExit -- delete ❌
 applyAction state "\ESC[A" = decreaseSelection state >>= returnWithoutExit -- selection up ☑️
 applyAction state "\ESC[B" = increaseSelection state >>= returnWithoutExit -- selection down ☑️
 applyAction state "\n" = enterDirectory state >>= returnWithoutExit -- enter directory ☑️
